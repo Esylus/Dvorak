d4a42697b0e1c95a8a89d47d695d8cad9c3dd0ba
PeterJoelMiller <peterjoelmiller@gmail.com>
2017-12-07 20:12:14 -0700

    Fingerings work with general color

---
+++ DvorakTrainer/Entities/Fingerings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DvorakTrainer.Entities
+{
+    public class Fingerings
+    {
+
+        public int O { get; set; }
+        public int R { get; set; }
+        public int G { get; set; }
+        public int B { get; set; }
+
+        public Fingerings(int o, int r, int g, int b)
+        {
+            O = o;
+            R = r;
+            G = g;
+            B = b;
+        }
+
+    }
+}
